9d3d21ef14aa7c16372ba866aac86421639dbbaf
namtaegyu <54826784+namtaegyu@users.noreply.github.com>
2019-12-17 12:05:28 +0900

    Add files via upload

---
+++ CSonDlg.cpp
@@ -0,0 +1,46 @@
+﻿// CSonDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Ftn1.h"
+#include "CSonDlg.h"
+#include "afxdialogex.h"
+#include "Ftn1Dlg.h"
+
+
+// CSonDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CSonDlg, CDialogEx)
+
+CSonDlg::CSonDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CSonDlg::~CSonDlg()
+{
+}
+
+void CSonDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CSonDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CSonDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CSonDlg 메시지 처리기
+
+void CSonDlg::OnBnClickedButton1()
+{
+	CMy20191030ClassDlg* p = (CMy20191030ClassDlg *)GetParent();
+	CString m;
+	GetDlgItemText(IDC_EDIT1, m);
+	if (!m.IsEmpty()) {
+		p->m_List.AddString(m);
+	}
+}
---
+++ Ftn1Dlg.h
@@ -0,0 +1,39 @@
+﻿
+// Ftn1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMy20191030ClassDlg 대화 상자
+class CMy20191030ClassDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMy20191030ClassDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MYFtn1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_List;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedCheck1();
+	afx_msg void OnLbnSelchangeList1();
+};
